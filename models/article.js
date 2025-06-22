f77bf04a3daa3b9ceb07bef610e702f43569bc9b
Arpit030 <126774517+Arpit030@users.noreply.github.com>
2025-06-22 13:31:07 +0530

    Update article.js

--- models/article.js
+++ models/article.js
@@ -4,7 +4,6 @@ const slugify = require('slugify');
 const createDomPurify = require('dompurify');
 const { JSDOM } = require('jsdom');
 const dompurify = createDomPurify(new JSDOM().window);
-
 const articleSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -31,15 +30,11 @@ const articleSchema = new mongoose.Schema({
     required: true
   }
 });
-
 articleSchema.pre('validate', function (next) {
-  if (this.title) {
+  if (this.title) 
     this.slug = slugify(this.title, { lower: true, strict: true });
-  }
-  if (this.markdown) {
+  if (this.markdown) 
     this.sanitizedHTML = dompurify.sanitize(marked(this.markdown));
-  }
   next();
 });
-
 module.exports = mongoose.model('Article', articleSchema);
